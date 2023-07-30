c5641fa527cd4a225ddb4a6de5ac329c75ef5690
prasanthrv98 <prasanthoffcl@gmail.com>
2023-07-30 17:37:12 +0530

    project init

---
+++ src/pages/user/User.tsx
@@ -0,0 +1,14 @@
+import Single from "../../components/single/Single"
+import { singleUser } from "../../data"
+
+const User = () => {
+  //Fetch data and send to Single Component
+
+  return (
+    <div className="user">
+      <Single {...singleUser} />
+    </div>
+  )
+}
+
+export default User
